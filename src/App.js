4162e24697a46012d391b1d8e4dd1472b6a5bd8e
Zaki Khairi <zakielbavarian@gmail.com>
2020-04-11 16:28:21 +0700

    init App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 import Home from "./pages/Home";
+import Shop from "./pages/Shop";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/Shop">
+          <Shop />
+        </Route>
+      </Switch>
+      </div>
+    </Router>
   );
 }
 

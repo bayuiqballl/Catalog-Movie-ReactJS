db8711192c1f5b0aaca1e6be3aa731e29fad7ad1
Zaki Khairi <zakielbavarian@gmail.com>
2020-04-11 16:26:34 +0700

    init header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,44 +6,44 @@ import Nav from "react-bootstrap/Nav";
 import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <Navbar collapseOnSelect expand="lg" className="my-red ">
-      <Navbar.Brand className="text-light" href="#home">
+      <Navbar.Brand className="text-light" to="/">
         Catalog Movie
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="navbar-nav text-center mx-auto order-0">
-          <Nav.Link className="pr-3 text-light" href="#features">
+          <Nav.Link className="pr-3 text-light" to="#features">
             Home
           </Nav.Link>
-          <Nav.Link className="pr-3 text-light" href="#pricing">
+          <Nav.Link className="pr-3 text-light" to="#pricing">
             Catalog
           </Nav.Link>
-          <Nav.Link className="pr-3 text-light" href="#pricing">
+          <Nav.Link className="pr-3 text-light" to="#pricing">
             News
           </Nav.Link>
-          <Nav.Link className="pr-3 text-light" href="#pricing">
+          <Nav.Link className="pr-3 text-light" to="#pricing">
             About
           </Nav.Link>
-          <Nav.Link className="pr-3 text-light" href="#pricing">
+          <Nav.Link className="pr-3 text-light" to="/Shop">
             Shop
           </Nav.Link>
-          <Nav.Link className="pr-3 text-light" href="#pricing">
+          <Nav.Link className="pr-3 text-light" to="#pricing">
             Contact Us
           </Nav.Link>
         </Nav>
         <Nav>
           <Button variant="danger" className="mr-3 py-0" size="sm">
-            <Nav.Link className="text-light" href="#deets">
+            <Nav.Link className="text-light" to="#deets">
               Sign In
             </Nav.Link>
           </Button>
           <Button variant="danger" className="mr-3 py-0" size="sm">
-            <Nav.Link className="text-light" href="#deets">
+            <Nav.Link className="text-light" to="#deets">
               Sign Up
             </Nav.Link>
           </Button>

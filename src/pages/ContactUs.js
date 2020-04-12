6f65cc47a718406c6652dc55daf0d25fd8ac7e61
Zaki Khairi <zakielbavarian@gmail.com>
2020-04-12 12:31:04 +0700

    init Contact Page

---
+++ src/pages/ContactUs.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import ContactHeader from '../components/ComponentContact/ContactHeader';
+import ContactSection from '../components/ComponentContact/ContactSection';
+
+const ContactUs = () => {
+    return (
+        <div>
+            <ContactHeader/>
+            <ContactSection/>
+        </div>
+    )
+}
+
+export default ContactUs;

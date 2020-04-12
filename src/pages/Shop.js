52427856bd1109eb3331dd00f45064558788e0c8
Zaki Khairi <zakielbavarian@gmail.com>
2020-04-12 12:31:21 +0700

    init Shop Page

---
+++ src/pages/Shop.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import ProductCatalog from '../components/ComponentShop/ProductCatalog';
+import ShopHeader from '../components/ComponentShop/ShopHeader';
+
+const Shop = () => {
+    return (
+        <div>
+            <ShopHeader/>
+            <ProductCatalog/>
+            
+        </div>
+    )
+}
+
+export default Shop;
